ca0855cdeffa0a7cbada7e73c2bfb69a67afaea0
Vladimir Kovalyov <vladimir.kovalyov2002@gmail.com>
2019-12-04 19:47:40 +0500

    Second Day. Сделан вывод тредов и постов из БД.

--- QuiteHouse_board/Controllers/HomeController.cs
+++ QuiteHouse_board/Controllers/HomeController.cs
@@ -1,23 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using QuiteHouse_board.Model.Context;
+using QuiteHouse_board.Models;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using QuiteHouse_board.Model.Context;
-using QuiteHouse_board.Models;
 
 namespace QuiteHouse_board.Controllers
 {
     public class HomeController : Controller
     {
+        public List<Boards> Boards { get; set; }
+        public List<Threads> Threads { get; set; }
+
+        public HomeController()
+        {
+            LoadBoard();
+        }
         public IActionResult Index()
+        {
+            ViewBag.Boards = Boards;
+            ViewBag.Threads = Threads;
+            return View();
+        }
+
+        private void LoadBoard()
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                ViewBag.boards = db.Boards.ToList();
+                Boards = db.Boards.ToList();
+                Threads = db.Threads.ToList();
+
+                foreach (Threads t in Threads)
+                {
+                    t.Posts = db.Posts.Where(x => x.ThreadId == t.Id).ToList();
+                }
             }
-            return View();
         }
 
         public IActionResult Privacy()
